bbc06a39a9001e108ebb9bb9a71ab572ca1f5922
Christopher Kok <ckok@purdue.edu>
2019-10-04 19:57:29 -0500

    Initial Push
    - initial sprites
    - level generator
    - simple 2d movement and camera follow

---
+++ LudumDare45/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public float speed = 2.0f;
+    private Vector3 pos;
+    private Transform tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+        tr = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D) && tr.position == pos) {
+            pos += Vector3.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && tr.position == pos) {
+            pos += Vector3.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && tr.position == pos) {
+            pos += Vector3.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && tr.position == pos) {
+            pos += Vector3.down;
+        }
+        
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+    }
+}
